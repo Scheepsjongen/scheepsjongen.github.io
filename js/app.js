b04ec401a969ca91000dc3d4f11d825572d6df1c
Oleh Shklyar <oleh.shklyar@gmail.com>
2021-02-07 13:24:35 +0200

    Formatting

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-window.addEventListener("load",()=>{"serviceWorker"in navigator&&navigator.serviceWorker.register("./sw.js").then(e=>{console.log("Service worker successfully registered",e)}).catch(e=>{console.log("Service worker registration failed",e)})});
+window.addEventListener("load", () => {
+    "serviceWorker" in navigator&&navigator.serviceWorker.register("./sw.js").then(e => {
+        console.log("Service worker successfully registered",e)
+    }).catch(e => {
+        console.log("Service worker registration failed",e)
+    })
+});
--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
-const staticCacheName = "static-cache-v0", dynamicCacheName = "dynamic-cache-v0",
-    staticAssets = [ 
-        "index.html", 
+const
+    staticCacheName = "static-cache-v0",
+    dynamicCacheName = "dynamic-cache-v0",
+    staticAssets = [
+        "index.html",
         "/list/",
         "/img/*",
         "/quiz/img/*",
@@ -9,4 +11,31 @@ const staticCacheName = "static-cache-v0", dynamicCacheName = "dynamic-cache-v0"
         "/js/*",
     ];
 
-async function checkCache(e){return await caches.match(e)||checkOnline(e)}async function checkOnline(e){const a=await caches.open(dynamicCacheName);try{const c=await fetch(e);return await a.put(e,c.clone()),c}catch(c){const t=await a.match(e);return t||(-1!==e.url.indexOf(".html")?caches.match("./index.html"):caches.match("./img/icons/back.png"))}}self.addEventListener("install",async e=>{const a=await caches.open(staticCacheName);await a.addAll(staticAssets),console.log("Service worker has been installed")}),self.addEventListener("activate",async e=>{const a=(await caches.keys()).map(async e=>{[staticCacheName,dynamicCacheName].includes(e)||await caches.delete(e)});await Promise.all(a),console.log("Service worker has been activated")}),self.addEventListener("fetch",e=>{console.log(`Trying to fetch ${e.request.url}`),e.respondWith(checkCache(e.request))}); 
+async function checkCache(e) {
+    return await caches.match(e) || checkOnline(e)
+}
+
+async function checkOnline(e) {
+    const a = await caches.open(dynamicCacheName);
+    try {
+        const c = await fetch(e);
+        return await a.put(e,c.clone()),c
+    } catch(c) {
+        const t = await a.match(e);
+        return t || (-1 !== e.url.indexOf(".html") ? caches.match("./index.html") : caches.match("./img/icons/back.png"))
+    }
+}
+
+self.addEventListener("install", async e => {
+    const a = await caches.open(staticCacheName);
+    await a.addAll(staticAssets), console.log("Service worker has been installed")
+}),
+self.addEventListener("activate", async e => {
+    const a = (await caches.keys()).map(async e => {
+        [staticCacheName, dynamicCacheName].includes(e) || await caches.delete(e)
+    });
+    await Promise.all(a),console.log("Service worker has been activated")
+}),
+self.addEventListener("fetch", e => {
+    console.log(`Trying to fetch ${e.request.url}`), e.respondWith(checkCache(e.request))
+});
