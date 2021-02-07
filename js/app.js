a8fa4d3db95fb19a5d1c12fc5dbb6b1b2a089f40
Oleh Shklyar <oleh.shklyar@gmail.com>
2021-02-07 13:36:18 +0200

    Moved sw.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
-    "serviceWorker" in navigator&&navigator.serviceWorker.register("./sw.js").then(e => {
-        console.log("Service worker successfully registered",e)
+    "serviceWorker" in navigator&&navigator.serviceWorker.register("/js/sw.js").then(e => {
+        console.log("Service worker successfully registered", e)
     }).catch(e => {
-        console.log("Service worker registration failed",e)
+        console.log("Service worker registration failed", e)
     })
 });
